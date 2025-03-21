db6ebfb791928607d738d53f6892151bbb0fe1ee
Nikita318-creator <nikuvar77@gmail.com>
2025-03-21 19:47:59 +0300

    Добавил логирование webhook-запросов

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -27,7 +27,7 @@ public func configure(_ app: Application) async throws {
         let update = try req.content.decode(Update.self) // Здесь Update — это структура для данных Telegram
 
         // Ты можешь обработать входящие сообщения в этой части
-        print("Получено обновление: \(update)") // Например, просто выводим обновление
+        app.logger.info("Получено обновление: \(update)")
 
         // Возвращаем успешный ответ
         return .ok
